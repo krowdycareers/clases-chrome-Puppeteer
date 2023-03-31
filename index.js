c602a9e5f4481ae6dcef92b9b4dd69ce609f260e
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2023-03-30 19:31:56 -0500

    [feat] copy function from chorme extension on oage.evaluate

--- index.js
+++ index.js
@@ -4,4 +4,37 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://www.occ.com.mx/empleos/");
+
+  const jobs = await page.evaluate(function getJobInformation() {
+    const elemCardJobs = [...document.querySelectorAll('[id*="jobcard-"]')];
+    const jobs = elemCardJobs.map((cardJob) => {
+      const [
+        { href: url },
+        {
+          children: [
+            {
+              children: [
+                { innerText: fecha },
+                { innerText: title },
+                { innerText: salario },
+                { innerText: beneficios },
+                {},
+                {
+                  children: [elementEmpresCiudad],
+                },
+              ],
+            },
+          ],
+        },
+      ] = cardJob.children;
+
+      const empresa = elementEmpresCiudad?.querySelector("label")?.innerText;
+      const ciudad = elementEmpresCiudad?.querySelector("p")?.innerText;
+      return { url, fecha, title, salario, beneficios, empresa, ciudad };
+    });
+
+    return jobs;
+  });
+
+  console.log(JSON.stringify(jobs, null, 2));
 })();
